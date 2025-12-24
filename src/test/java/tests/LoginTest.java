467a0e3ef524d58a339b49cddde60361d0ee42bf
CongHuy <truongconghuy1002@gmail.com>
2025-12-24 18:13:53 +0700

    Add login test script - Test Case ID: LILO-03

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -5,14 +5,22 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 import static java.lang.Thread.sleep;
 
 public class LoginTest {
 
     ChromeDriver CD;
+    WebDriverWait wait ;
     @BeforeMethod
     public void setup(){
 
@@ -21,16 +29,19 @@ public class LoginTest {
         CD.get("http://localhost:1002/orangehrm/web/index.php/auth/login");
     }
 
+    @AfterTest
+    public void clean(){
+        CD.quit();
+    }
+
     @Test
     // Test Case: LILO-02 - Verify login successfully with valid credentials
     public void loginSuccessfullyWithValidCredentials() {
         String Username = "Admin";
         String Password = "0123456Huy***";
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        // wait chờ tối đa 10s cho đến khi ô username hiển thị trên UI trước khi thao tác
+        wait = new WebDriverWait(CD, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
         WebElement inputUS = CD.findElement(By.name("username"));
         inputUS.sendKeys(Username);
         WebElement inputPS = CD.findElement(By.name("password"));
@@ -38,4 +49,24 @@ public class LoginTest {
         WebElement btLogin = CD.findElement(By.cssSelector("button[type='submit']"));
         btLogin.click();
     }
+
+    @Test
+    // Test Case: LILO-03 - Verify system shows error message when login with username only (password is empty)
+    public void loginWithUsernameOnly_ShowErrorMessage(){
+        String Username = "Admin";
+        String errormessagePS = "Required";
+        // wait chờ tối đa 10s cho đến khi ô username hiển thị trên UI trước khi thao tác
+        wait = new WebDriverWait(CD, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
+        WebElement inputUS = CD.findElement(By.name("username"));
+        inputUS.sendKeys(Username);
+        WebElement btLogin = CD.findElement(By.cssSelector("button[type='submit']"));
+        btLogin.click();
+        WebElement messagePS = CD.findElement(By.cssSelector("span[class='oxd-text oxd-text--span oxd-input-field-error-message oxd-input-group__message'"));
+        String messageps = messagePS.getText();
+        Assert.assertEquals(messageps, errormessagePS);
+        System.out.println(messageps);
+        System.out.println(errormessagePS);
+    }
+
 }
