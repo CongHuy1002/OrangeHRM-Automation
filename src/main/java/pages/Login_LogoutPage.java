774bfc47f8e64139feb37c13bd086ea03ac0cd17
CongHuy <truongconghuy1002@gmail.com>
2025-12-27 23:08:53 +0700

    Refactor Login/Logout automation: improve Page Object, reuse actions, clean tests

---
+++ src/main/java/pages/Login_LogoutPage.java
@@ -0,0 +1,68 @@
+package pages;
+
+import base.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Login_LogoutPage extends BasePage {
+
+    public Login_LogoutPage(WebDriver driver) {
+        super(driver);
+    }
+
+    // Locators
+    private By usernameInput = By.name("username");
+    private By passwordInput = By.name("password");
+    private By loginButton   = By.cssSelector("button[type='submit']");
+    private By logoutButton   = By.xpath("//a[text()='Logout']");
+    private By avatarButton = By.cssSelector("span[class='oxd-userdropdown-tab']");
+    private By errorMessage  = By.cssSelector("span.oxd-input-field-error-message");
+    private By errorMessages  = By.cssSelector("span.oxd-input-field-error-message");
+    private By invalidAlert  = By.cssSelector("p.oxd-alert-content-text");
+
+    // Actions
+    public void enterUsername(String username) {
+        type(usernameInput, username);
+    }
+
+    public void enterPassword(String password) {
+        type(passwordInput, password);
+    }
+
+    public void clickLogin() {
+        click(loginButton);
+    }
+
+    public void login(String username, String password) {
+        enterUsername(username);
+        enterPassword(password);
+        clickLogin();
+    }
+
+    public void logout(){
+        click(avatarButton);
+        click(logoutButton);
+    }
+
+    public String getInvalidLoginMessage() {
+        return getText(invalidAlert);
+    }
+
+    public String getErrorMessage(){
+        return getText(errorMessage);
+    }
+
+    public List<String> getAllErrorMessages() {
+        List<WebElement> elements = driver.findElements(errorMessages);
+        List<String> messages = new ArrayList<>();
+
+        for (WebElement el : elements) {
+            messages.add(el.getText());
+        }
+        return messages;
+    }
+}
--- src/test/java/tests/Login_LogoutTest.java
+++ src/test/java/tests/Login_LogoutTest.java
@@ -1,173 +1,99 @@
 package tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pages.Login_LogoutPage;
 
-import java.time.Duration;
 import java.util.List;
 
-import static java.lang.Thread.sleep;
 
 public class Login_LogoutTest {
 
-    ChromeDriver CD;
+    WebDriver driver;
     WebDriverWait wait ;
+    Login_LogoutPage login_logoutPage;
+
+    String Username = "Admin";
+    String Password = "0123456Huy***";
+    String UsernameWrong = "Admin12";
+    String PasswordWrong = "0123456HUy***";
+    String errormessageUS = "Required";
+    String errormessagePS = "Required";
+    String invalidmessage = "Invalid credentials";
+
     @BeforeMethod
     public void setup(){
 
         WebDriverManager.chromedriver().setup();
-        CD = new ChromeDriver();
-        CD.get("http://localhost:1002/orangehrm/web/index.php/auth/login");
+        driver = new ChromeDriver();
+        driver.get("http://localhost:1002/orangehrm/web/index.php/auth/login");
+        login_logoutPage = new Login_LogoutPage(driver);
     }
 
-    @AfterTest
+    @AfterMethod
     public void clean(){
-        CD.quit();
+        driver.quit();
     }
 
     @Test
     // Test Case: LILO-02 - Verify login successfully with valid credentials
     public void loginSuccessfullyWithValidCredentials() {
-        String Username = "Admin";
-        String Password = "0123456Huy***";
-        // wait chờ tối đa 10s cho đến khi ô username hiển thị trên UI trước khi thao tác
-        wait = new WebDriverWait(CD, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
-        WebElement inputUS = CD.findElement(By.name("username"));
-        inputUS.sendKeys(Username);
-        WebElement inputPS = CD.findElement(By.name("password"));
-        inputPS.sendKeys(Password);
-        WebElement btLogin = CD.findElement(By.cssSelector("button[type='submit']"));
-        btLogin.click();
+        login_logoutPage.enterUsername(Username);
+        login_logoutPage.enterPassword(Password);
+        login_logoutPage.clickLogin();
     }
 
     @Test
     // Test Case: LILO-03 - Verify system shows error message when login with username only (password is empty)
     public void loginWithUsernameOnly_ShowErrorMessage(){
-        String Username = "Admin";
-        String errormessagePS = "Required";
-        // wait chờ tối đa 10s cho đến khi ô username hiển thị trên UI trước khi thao tác
-        wait = new WebDriverWait(CD, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
-        WebElement inputUS = CD.findElement(By.name("username"));
-        inputUS.sendKeys(Username);
-        WebElement btLogin = CD.findElement(By.cssSelector("button[type='submit']"));
-        btLogin.click();
-        WebElement messagePS = CD.findElement(By.cssSelector("span[class='oxd-text oxd-text--span oxd-input-field-error-message oxd-input-group__message'"));
-        messagePS.getText();
-        Assert.assertEquals(messagePS.getText(), errormessagePS);
-        System.out.println(messagePS.getText());
-        System.out.println(errormessagePS);
+        login_logoutPage.enterUsername(Username);
+        login_logoutPage.clickLogin();
+        Assert.assertEquals(login_logoutPage.getErrorMessage(),errormessagePS);
     }
 
     @Test
     // Test Case: LILO-04 - Verify system shows error message when login with password only (admin is empty)
     public void loginWithPasswordOnly_ShowErrorMessage(){
-        String Password = "0123456Huy***";
-        String errormessageUS = "Required";
-        // wait chờ tối đa 10s cho đến khi ô username hiển thị trên UI trước khi thao tác
-        wait = new WebDriverWait(CD, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("password")));
-        WebElement inputPS = CD.findElement(By.name("password"));
-        inputPS.sendKeys(Password);
-        WebElement btLogin = CD.findElement(By.cssSelector("button[type='submit']"));
-        btLogin.click();
-        WebElement messageUS = CD.findElement(By.cssSelector("span[class='oxd-text oxd-text--span oxd-input-field-error-message oxd-input-group__message']"));
-        messageUS.getText();
-        Assert.assertEquals(messageUS.getText(),errormessageUS);
-        System.out.println(messageUS.getText());
-        System.out.println(errormessageUS);
+        login_logoutPage.enterPassword(Password);
+        login_logoutPage.clickLogin();
+        Assert.assertEquals(login_logoutPage.getErrorMessage(),errormessageUS);
     }
 
     @Test
     // Test Case: LILO-05 - Verify system shows error message when login with admin and password is empty
     public void loginWithAdminandPasswordIsEmpty_ShowErrorMessage(){
-        String errormessage = "Required";
-        // wait chờ tối đa 10s cho đến khi ô username hiển thị trên UI trước khi thao tác
-        wait = new WebDriverWait(CD, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[type='submit']")));
-        WebElement btLogin = CD.findElement(By.cssSelector("button[type='submit']"));
-        btLogin.click();
-
-        List<WebElement> messages = CD.findElements(By.cssSelector("span[class='oxd-text oxd-text--span oxd-input-field-error-message oxd-input-group__message']"));
-
-        int count = 0;
-        for (int i = 0; i < messages.size(); i ++){
-            Assert.assertEquals(messages.get(i).getText(),errormessage);
-        }
+        login_logoutPage.clickLogin();
+        List<String> messages = login_logoutPage.getAllErrorMessages();
+        Assert.assertEquals(messages.size(),2);
+        Assert.assertTrue(messages.contains(errormessageUS));
     }
 
     @Test
     // Test Case: LILO-06 - Verify system shows error message when login with admin is invalid
     public void loginWithAdminInvalid_ShowErrorMessage(){
-        String errormessage = "Invalid credentials";
-        String Username = "Admin12";
-        String Password = "0123456Huy***";
-        // wait chờ tối đa 10s cho đến khi ô username hiển thị trên UI trước khi thao tác
-        wait = new WebDriverWait(CD, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
-        WebElement inputUS = CD.findElement(By.name("username"));
-        inputUS.sendKeys(Username);
-        WebElement inputPS = CD.findElement(By.name("password"));
-        inputPS.sendKeys(Password);
-        WebElement btLogin = CD.findElement(By.cssSelector("button[type='submit']"));
-        btLogin.click();
-        wait = new WebDriverWait(CD, Duration.ofSeconds(5));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("p[class='oxd-text oxd-text--p oxd-alert-content-text']")));
-        WebElement message = CD.findElement(By.cssSelector("p[class='oxd-text oxd-text--p oxd-alert-content-text']"));
-        Assert.assertEquals(message.getText(),errormessage);
+        login_logoutPage.login(UsernameWrong,Password);
+        Assert.assertEquals(login_logoutPage.getInvalidLoginMessage(),invalidmessage);
     }
 
     @Test
     // Test Case: LILO-07 - Verify system shows error message when login with password is invalid
     public void loginWithPasswordInvalid_ShowErrorMessage(){
-        String errormessage = "Invalid credentials";
-        String Username = "Admin";
-        String Password = "0123456HUy***";
-        // wait chờ tối đa 10s cho đến khi ô username hiển thị trên UI trước khi thao tác
-        wait = new WebDriverWait(CD, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
-        WebElement inputUS = CD.findElement(By.name("username"));
-        inputUS.sendKeys(Username);
-        WebElement inputPS = CD.findElement(By.name("password"));
-        inputPS.sendKeys(Password);
-        WebElement btLogin = CD.findElement(By.cssSelector("button[type='submit']"));
-        btLogin.click();
-        wait = new WebDriverWait(CD, Duration.ofSeconds(5));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("p[class='oxd-text oxd-text--p oxd-alert-content-text']")));
-        WebElement message = CD.findElement(By.cssSelector("p[class='oxd-text oxd-text--p oxd-alert-content-text']"));
-        Assert.assertEquals(message.getText(),errormessage);
+        login_logoutPage.login(Username,PasswordWrong);
+        Assert.assertEquals(login_logoutPage.getInvalidLoginMessage(),invalidmessage);
     }
 
     @Test
     // Test Case: LILO-08 - Verify logout successfully after login
     public void logoutSuccessfullyAfterLogin(){
-        String Username = "Admin";
-        String Password = "0123456Huy***";
-        // wait chờ tối đa 10s cho đến khi ô username hiển thị trên UI trước khi thao tác
-        wait = new WebDriverWait(CD, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
-        WebElement inputUS = CD.findElement(By.name("username"));
-        inputUS.sendKeys(Username);
-        WebElement inputPS = CD.findElement(By.name("password"));
-        inputPS.sendKeys(Password);
-        WebElement btLogin = CD.findElement(By.cssSelector("button[type='submit']"));
-        btLogin.click();
-        wait = new WebDriverWait(CD, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span[class='oxd-userdropdown-tab']")));
-        WebElement buttonAvatar = CD.findElement(By.cssSelector("span[class='oxd-userdropdown-tab']"));
-        buttonAvatar.click();
-        WebElement buttonLogout = CD.findElement(By.xpath("//a[text()='Logout']"));
-        buttonLogout.click();
-
+        login_logoutPage.login(Username,Password);
+        login_logoutPage.logout();
     }
 
 }
